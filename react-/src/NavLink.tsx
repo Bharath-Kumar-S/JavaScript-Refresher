912ff2c95b77d511a437f23e2d838751a7a7e178
Bharath Kumar <bharathkumarkarthick@gmail.com>
2024-11-24 00:54:30 +0200

    Create NavLink.tsx

---
+++ react-/src/NavLink.tsx
@@ -0,0 +1,33 @@
+// import { BrowserRouter as Router, Routes, Route, NavLink } from "react-router-dom";
+// import Home from './Home';
+// import Stars from './Stars';
+
+// const App = () => (
+//   <Router>
+//     <nav>
+//       <NavLink
+//         to="/"
+//         end // Ensures only the exact path "/" is matched
+//         style={({ isActive }) => ({
+//           color: isActive ? 'cyan' : 'grey'
+//         })}
+//       >
+//         Home
+//       </NavLink>
+//       <NavLink
+//         to="/stars"
+//         style={({ isActive }) => ({
+//           color: isActive ? 'cyan' : 'grey'
+//         })}
+//       >
+//         Stars
+//       </NavLink>
+//     </nav>
+//     <Routes>
+//       <Route path="/" element={<Home />} />
+//       <Route path="/stars" element={<Stars />} />
+//     </Routes>
+//   </Router>
+// );
+
+// export default App;
